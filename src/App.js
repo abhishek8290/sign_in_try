b8503fd5eee8baa750c569eed6dec2ff8c7ebb49
abhishek8290 <abhishek8290@gmail.com>
2019-11-12 19:43:11 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+import { Button, Input, Checkbox } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Popup extends React.Component {
+  render() {
+    return (
+      
+        <div>
+          <div> username </div>
+        <Input
+          name='username'
+          placeholder='Username'
+          // onChange={e => this.onChange(e)}
+          value={1} />
+          
+          <div> EmailId </div>
+          
+        <Input
+          name='email'
+          placeholder='Email'
+          // onChange={e => this.onChange(e)}
+          value={1} />
+          <div> Password </div>
+        <Input
+          name='password'
+          placeholder='Password'
+          type='password'
+          // onChange={e => this.onChange(e)}
+          value={1} />
+       
+       
+       </div>
+    
+      
+    );
+  }
 }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false
+    };
+  }
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+  render() {
+    return (
+      <div className='app'>
+        <h1>hihi</h1>
+        <button onClick={this.togglePopup.bind(this)}>SIGN UP</button>
+        
+        <p>Random Statement to write </p>
+        {this.state.showPopup ? 
+          <Popup
+            text='Close Me'
+            closePopup={this.togglePopup.bind(this)}
+          />
+          : null
+        }
+      </div>
+    );
+  }
+};
 
 export default App;
