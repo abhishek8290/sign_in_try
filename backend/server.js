dcfa4583cd3e6667a36b0bf546840fc33941c976
abhishek8290 <abhishek8290@gmail.com>
2019-11-20 18:34:54 +0530

    backend api

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const users = require("./routes/api/users");
+
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+
+
+// if (process.env.NODE_ENV === "production") {
+ 
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './index.css';
-=======
-// import './index.css';
-
->>>>>>> fde372021614610783cc7f71a92d558b933e1d81
 import * as serviceWorker from './serviceWorker';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
