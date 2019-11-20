ed905bd54cf7f34934438197b807b958b7dff2d0
abhishek8290 <abhishek8290@gmail.com>
2019-11-20 18:36:42 +0530

    left over additon

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://127.0.0.1:27017/usertry2",
+  mongoURI: "mongodb://127.0.0.1:27017/usertry2",// here write your own mongodb uri
   secretOrKey: "secret"
 };
